6b1143697a942460bae70cd63ad8c137cfaf7c4f
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-03-08 22:44:00 +0000

    A little more progress. Need to fine tune getting the exact room name

--- tutorial/tutorial/spiders/spideyquotes_spider.py
+++ tutorial/tutorial/spiders/spideyquotes_spider.py
@@ -3,57 +3,62 @@ https://blog.scrapinghub.com/2016/04/20/scrapy-tips-from-the-pros-april-2016-edi
 '''
 
 import scrapy
+from scrapy.selector import Selector
 
 class SpidyQuotesViewStateSpider(scrapy.Spider):
-    name = "spidyquotes"
-    start_urls = ['http://studentssp.wit.ie/Timetables/RoomTT.aspx']
-    download_delay = 1.5
+	name = "spidyquotes"
+	start_urls = ['http://studentssp.wit.ie/Timetables/RoomTT.aspx']
+	download_delay = 1.5
 
-    # def parse(self, response):
-    #     room = response.css('select#CboLocation > option ::attr(value)').extract_first()
-        # starttime = '6'
-        # endtime = '7'
-    #     weeks = '28'
-    #     school = '%'
-    #     dept = '%'
+	# def parse(self, response):
+	#     room = response.css('select#CboLocation > option ::attr(value)').extract_first()
+		# starttime = '6'
+		# endtime = '7'
+	#     weeks = '28'
+	#     school = '%'
+	#     dept = '%'
 
-    #     yield scrapy.FormRequest(
-    #         'http://studentssp.wit.ie/Timetables/RoomTT.aspx',
-    #         formdata={
-    #             'cboSchool': school,
-    #             'CboWeeks': weeks,
-    #             'CboDept': dept,
-    #             'CboStartTime': starttime,
-    #             'CboEndTime': endtime,
-    #             'CboLocation': room,
-    #             '__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first()
-    #         },
-    #         callback=self.parse_r
-    #     )
+	#     yield scrapy.FormRequest(
+	#         'http://studentssp.wit.ie/Timetables/RoomTT.aspx',
+	#         formdata={
+	#             'cboSchool': school,
+	#             'CboWeeks': weeks,
+	#             'CboDept': dept,
+	#             'CboStartTime': starttime,
+	#             'CboEndTime': endtime,
+	#             'CboLocation': room,
+	#             '__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first()
+	#         },
+	#         callback=self.parse_r
+	#     )
 
-    def parse(self, response):
-        starttime = '6'
-        endtime = '7'
-        for room in response.css('select#CboLocation > option ::attr(value)').extract():
-            yield scrapy.FormRequest.from_response(
-                response,
-                formdata={'CboLocation': room, 'CboStartTime': starttime,
-                'CboEndTime': endtime},
-                callback=self.parse_results,
-            )
+	def parse(self, response):
+		starttime = '6'
+		endtime = '7'
+		for room in response.css('select#CboLocation > option ::attr(value)').extract():
+			yield scrapy.FormRequest.from_response(
+				response,
+				formdata={'CboLocation': room},
+				callback=self.parse_results,
+			)
 
-    def parse_r(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+	def parse_r(self, response):
+		page = response.url.split("/")[-2]
+		filename = 'quotes-%s.html' % page
+		with open(filename, 'wb') as f:
+			f.write(response.body)
+		self.log('Saved file %s' % filename)
 
-    def parse_results(self, response):
-        table = response.css("div#divTT > table > tbody > tr > td > b ::text")
-        # table = response.css("div#divTT > table > tbody > tr > td > b ::text")
-
-        # for quote in response.css("div#divTT"):
-        yield {
-            'room': table,
-        }
+	def parse_results(self, response):
+		# table = response.xpath('td::attr(align)').extract
+		# table = response.css("div#divTT > table > tbody > tr ::text")
+		# sel.xpath('//table[@class="bgWhite listTable"]//h3')
+		# for quote in response.css("div#divTT"):
+		# items = hxs.select('//table[@class="tableh/td')
+		hxs = Selector(response)
+		rows = hxs.xpath('//tr')
+		for row in rows:
+			print row.select('td/text()').extract()
+		# yield {
+		# 	'room': table,
+		# }
