843272bf23cd02da3be638740d1a5ab1e5077ab0
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-03-08 22:08:41 +0000

    Major progress

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -9,17 +9,32 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes" #Name of spider. Must be unique within a project
 
     
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
-    ]
+    # start_urls = [
+    #     'http://quotes.toscrape.com/page/1/',
+    #     'http://quotes.toscrape.com/page/2/',
+    # ]
      
 
+    # def parse(self, response):
+    #     for quote in response.css('div.quote'):
+    #         yield {
+    #             'text': quote.css('span.text::text').extract_first(),
+    #             'author': quote.css('small.author::text').extract_first(),
+    #             'tags': quote.css('div.tags a.tag::text').extract(),
+    #         }
+
+
+    def start_requests(self):
+            urls = [
+                'http://studentssp.wit.ie/Timetables/POSTT.aspx',
+            ]
+            for url in urls:
+                yield scrapy.Request(url=url, callback=self.parse)
+
     def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').extract_first(),
-                'author': quote.css('small.author::text').extract_first(),
-                'tags': quote.css('div.tags a.tag::text').extract(),
-            }
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
         
