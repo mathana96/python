ae836756d29159d6dc233040559cbe44a7af511e
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-04-21 22:12:05 +0100

    Can't click login

--- link_click.py
+++ link_click.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
-browser.get('http://seleniumhq.org/')
+browser.get('https://www.packtpub.com/index')
+
+elm = browser.find_element_by_class_name('login-popup')
+elm.click()
